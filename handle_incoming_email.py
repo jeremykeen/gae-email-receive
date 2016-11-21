e63c79a8649ee1f303c47ea27e4f3d46e82d4c36
Jeremy Keen <jeremykeen@gmail.com>
2016-11-21 14:42:25 -0500

    relocated write_bucket function

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -18,37 +18,45 @@ import os
 import cloudstorage as gcs
 import webapp2
 
-
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api import app_identity
 
-#[START retries]
+# [START retries]
 my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
                                           max_delay=5.0,
                                           backoff_factor=2,
                                           max_retry_period=15)
 gcs.set_default_retry_params(my_default_retry_params)
-#[END retries]
 
+
+# [END retries]
+
+# [START write_to_default_bucket]
+def write_file(self, filename, content):
+    bucket_name = os.environ.get('BUCKET_NAME',
+                                 app_identity.get_default_gcs_bucket_name())
+    bucket = '/' + bucket_name
+    file_location = bucket + filename
+    logging.info("File location: ", file_location)
+    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+    gcs_file = gcs.open(file_location,
+                        'w',
+                        options={'x-goog-meta-location': 'driveway',
+                                 'x-goog-meta-bar': 'bar'},
+                        retry_params=write_retry_params)
+    gcs_file.write(content.decode())
+    gcs_file.close()
+    logging.info("image uploaded to cloud storage")
+
+
+# [END write_to_default_bucket]
 
 
 class LogSenderHandler(InboundMailHandler):
-    # [START get_default_bucket]
-    def get(self):
-        bucket_name = os.environ.get('BUCKET_NAME',
-                                     app_identity.get_default_gcs_bucket_name())
-
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Demo GCS Application running from Version: '
-                            + os.environ['CURRENT_VERSION_ID'] + '\n')
-        self.response.write('Using bucket name: ' + bucket_name + '\n\n')
-
-    # [END get_default_bucket]
-
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
-# [END log_sender_handler]
-# [START bodies]
+        # [END log_sender_handler]
+        # [START bodies]
         plaintext_bodies = mail_message.bodies('text/plain')
         html_bodies = mail_message.bodies('text/html')
 
@@ -58,27 +66,16 @@ class LogSenderHandler(InboundMailHandler):
         try:
             if hasattr(mail_message, 'attachments'):
                 for filename, content in mail_message.attachments:
-                    bucket = '/' + bucket_name
-                    file_location = bucket + filename
-                    self.response.write('Creating file %s\n' % filename)
-
-                    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-                    gcs_file = gcs.open(file_location,
-                                        'w',
-                                        options={'x-goog-meta-foo': 'foo',
-                                                 'x-goog-meta-bar': 'bar'},
-                                        retry_params=write_retry_params)
-                    gcs_file.write(content.decode())
-                    gcs_file.close()
-                    logging.info("image uploaded to cloud storage")
+                    write_file(filename, content)
         except:
             logging.exception("Exception decoding attachments in email from %s" % mail_message.sender)
             # ...
-# [END bodies]
+            # [END bodies]
             logging.info("Html body of length %d.", len(decoded_html))
         for content_type, body in plaintext_bodies:
             plaintext = body.decode()
             logging.info("Plain text body of length %d.", len(plaintext))
+
 
 # [START app]
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
